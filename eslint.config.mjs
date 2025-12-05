30bfaca1819a052f678e0cdd121c1eb3be023685
whdgur5717 <whdgur5717@gmail.com>
2025-12-05 16:06:56 +0900

    initial settings

---
+++ eslint.config.mjs
@@ -0,0 +1,47 @@
+import js from "@eslint/js"
+import json from "@eslint/json"
+import markdown from "@eslint/markdown"
+import { defineConfig } from "eslint/config"
+import eslintConfigPrettierFlat from "eslint-config-prettier/flat"
+import simpleImportSort from "eslint-plugin-simple-import-sort"
+import globals from "globals"
+import tseslint from "typescript-eslint"
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  tseslint.configs.recommended,
+  eslintConfigPrettierFlat,
+  {
+    files: ["**/*.json"],
+    plugins: { json },
+    language: "json/json",
+    extends: ["json/recommended"],
+  },
+  {
+    files: ["**/*.jsonc"],
+    plugins: { json },
+    language: "json/jsonc",
+    extends: ["json/recommended"],
+  },
+  {
+    files: ["**/*.md"],
+    plugins: { markdown },
+    language: "markdown/gfm",
+    extends: ["markdown/recommended"],
+  },
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "import/first": "error",
+    },
+  },
+])
