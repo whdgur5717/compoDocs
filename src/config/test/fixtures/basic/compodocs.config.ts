747e0b43522ca07f657a98cb854adc8f0439aa1a
whdgur5717 <whdgur5717@gmail.com>
2025-12-06 16:42:48 +0900

    chore(config): add fixture for test

---
+++ src/config/test/fixtures/basic/compodocs.config.ts
@@ -0,0 +1,17 @@
+export default {
+  workspace: {
+    include: ["packages/*", "apps/*"],
+    exclude: ["packages/legacy"],
+  },
+  commands: {
+    generate: {
+      files: ["**/*.tsx"],
+      outputDir: "__generated__",
+      tag: "generate",
+    },
+    // 나중에 추가
+    // build: {
+    //   outputDir: "docs"
+    // }
+  },
+}
---
+++ src/config/test/fixtures/wrong/compodocs.config.mjs
@@ -0,0 +1,18 @@
+export default {
+    workspaces: {
+      includes: ["packages/*", "apps/*"],
+      excludes: ["packages/legacy"],
+      wrong: []
+    },
+    commands: {
+      generate: {
+        files: ["**/*.tsx"],
+        outputDir: "__generated__",
+        tag: "generate",
+      },
+      // 나중에 추가
+      // build: {
+      //   outputDir: "docs"
+      // }
+    },
+  }
